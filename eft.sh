f52343581a74da15dd960ebe862d59ddee91e656
JPTomorrow <justin.parker.morrow@gmail.com>
2023-07-05 12:57:16 -0600

    added desc

--- eft.sh
+++ eft.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# A script to move all files of a certain type from subfolders to the current directory
+
 filetype=$1
 
 # get all folders in current directory
 for folder in */ ; do
     # get all files in each folder of type
     for file in "$folder"*.$filetype; do
-        # move mkv file to current directory
+        # move mkv file to current directory and delete folder
         mv "$file" .
+        rm -rf "$folder"
     done
 done
